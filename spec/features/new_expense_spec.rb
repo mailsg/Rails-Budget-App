edf64c4236a4bf4449240d9006e953b8ab8e3bfc
Sandeep Ghosh <mailsg0912@gmail.com>
2023-09-20 18:43:54 +0530

    Add unit and integration tests

---
+++ spec/features/new_expense_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+
+RSpec.describe User, type: :model do
+    context 'validations' do
+      it 'validates the presence of a name' do
+        user = User.create!(
+          name: 'Sandeep Ghosh',
+          email: 'mailsg0912@gmail.com',
+          password: 'pass1234',
+          password_confirmation: 'pass1234',
+          confirmed_at: Time.now
+        )
+  
+        expect(user).to be_valid
+  
+        user.name = nil
+        expect(user).not_to be_valid
+      end
+    end
+  
+    context 'associations' do
+      it 'has many categories' do
+        association = described_class.reflect_on_association(:category)
+        expect(association.macro).to eq(:has_many)
+      end
+  
+      it 'has many expenses' do
+        association = described_class.reflect_on_association(:expense)
+        expect(association.macro).to eq(:has_many)
+      end
+    end
+end
