ec956196b6d528f8fbdbc866942bcbf08eaef7bf
Sandeep Ghosh <mailsg0912@gmail.com>
2023-09-21 15:30:01 +0530

    Add deployment config files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,8 +17,6 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   config.action_mailer.default_url_options = { host: 'https://budgetapp-sandeep.onrender.com' }
-  
-  config.action_mailer.delivery_method = :letter_opener
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
